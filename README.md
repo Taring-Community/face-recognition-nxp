7b642b2dc26ad6782d28bdf6114c797204a8b2c0
Anh Dan <anhdan.do@gmail.com>
2023-04-14 10:00:40 +0700

    complete

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # face-recognition-nxp
-Face Recognition on NXP board, using SSD TFLite model for face detection
+Face Recognition on NXP board, using TFLite 8bit model for face detection
+
+## Prepare Development Environment
+
+If Linux OS
+
+``` sh
+conda env create -f tensorflow_env_linux.yaml -n tensorflow
+```
+
+If MAC OS M1
+
+``` sh
+conda env create -f tensorflow_env_macos.yaml -n tensorflow
+```
+
+## Convert ONNX model to TFLite model
+
+Follow the instruction in the file `src/onnx_to_tflite.ipynb` to convert the ONNX model to TFLite model.
+
+
+## Run the demo
+
+1. Copy the TFLite model from result folder to the `models/tflite8bit` folder.
+2. Change the directory to the model in the file `src/run_inference.py`.
+![image](data/tmp/screenshot1.png)
+3. Run the demo by the command 
+
+``` sh
+# inference with video
+python3 run_inference.py --video_path <video_path>
+```
+
+``` sh
+# inference with image
+python3 run_inference.py --image_path <image_path>
+```
---
+++ src/run_inference.py
@@ -0,0 +1,77 @@
+import argparse
+import cv2
+import time
+
+from TFLiteFaceDetector import UltraLightFaceDetecion
+
+
+parser = argparse.ArgumentParser(description='TFLite Face Detector')
+parser.add_argument('--img_path', type=str, help='Image path for inference')
+parser.add_argument('--video_path', type=str, help='Video path for inference')
+args = parser.parse_args()
+
+#
+# ===== Function to run inference on image =====
+#
+def image_inference(image_path, model_path, color=(125, 255, 0)):
+
+    fd = UltraLightFaceDetecion(model_path,
+                                conf_threshold=0.6)
+
+    img = cv2.imread(image_path)
+
+    boxes, scores = fd.inference(img)
+
+    for result in boxes.astype(int):
+        cv2.rectangle(img, (result[0], result[1]),
+                      (result[2], result[3]), color, 2)
+
+    cv2.imshow('res', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+#
+# ===== Function to run inference on video =====
+#
+def video_inference(video, model_path, color=(125, 255, 0)):
+
+    fd = UltraLightFaceDetecion(model_path,
+                                conf_threshold=0.88)
+
+    cap = cv2.VideoCapture(video)
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        start_time = time.perf_counter()
+        boxes, scores = fd.inference(frame)
+
+        for result in boxes.astype(int):
+            cv2.rectangle(frame, (result[0], result[1]),
+                          (result[2], result[3]), color, 2)
+
+        cv2.imshow('res', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+#
+# ===== MAIN FUNCTION =====
+#
+if __name__ == '__main__':
+
+    filepath = f"pretrained/version-RFB-320_int8.tflite"
+
+    if args.img_path:
+        image_inference(args.img_path, filepath)
+    elif args.video_path:
+        video_inference(args.video_path, filepath)
+    else:
+        print('--ima_path or --video_path must be filled')
